f043a0af4869d32a871ff5b3a02f6a435452b3d3
Phiwa <phiwen2@gmail.com>
2021-11-16 21:50:51 +0200

    moved components folder out of pages

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './Navbar';
-import '../../styles/global.css'
 
 const Layout = ({children}) => {
     return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import '../styles/global.css'
 
 const Navbar = () => {
     return (
