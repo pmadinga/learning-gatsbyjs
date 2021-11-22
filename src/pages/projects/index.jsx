5908d8fdbf0a6bdc0f13773aaf5ebbb2081c43c5
Phiwa <phiwen2@gmail.com>
2021-11-22 20:32:52 +0200

    Mapping through projects

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,16 +1,47 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
 import * as styles from '../../styles/projects.module.css'
 
-const Projects = () => {
+const Projects = ({data}) => {
+    console.log(data);
+
+    const projects = data.allMarkdownRemark.nodes;
     return (
         <Layout>
             <div className={styles.portfolio}>
                 <h1>Projects I have created</h1>
                 <h2>Have a look at all of the things i have created</h2>
+                <div className={styles.projects}>
+                    {projects.map(project => (
+                        <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
+                            <div>
+                                <h3>{project.frontmatter.title}</h3>
+                                <p>{project.frontmatter.stack}</p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
             </div>
         </Layout>
     );
 }
 
 export default Projects;
+
+// export page query
+export const query = graphql`
+query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          stack
+          slug
+        }
+        id
+      }
+    }
+  }
+  
+`
