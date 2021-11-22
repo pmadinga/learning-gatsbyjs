43d8ad4257480f4c69fb67bbc6127ed3fc7105e6
Phiwa <phiwen2@gmail.com>
2021-11-22 23:24:29 +0200

    Optimised queries

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
-import { Link } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React from "react"
 import Layout from '../components/Layout';
-import * as styles from '../styles/home.module.css'
+import * as styles from '../styles/home.module.css';
+import Img from "gatsby-image";
 
-export default function Home() {
-
+export default function Home({data}) {
+  console.log(data);
   return (
     <Layout>
       <section className={styles.header}>
@@ -14,9 +15,20 @@ export default function Home() {
           <p>UX Designer & web developer based in Gauteng</p>
           <Link className={styles.btn} to='/projects'>my portfolio projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}} />
+        <Img fluid={data.file.childImageSharp.fluid}/>
       </section>
     </Layout>
   )
 }
 
+export const query = graphql`
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+    } 
+  }
+}
+`
