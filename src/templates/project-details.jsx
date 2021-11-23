bb0be3ebb8ffaf7d5deb40ad3b830dfed6383333
Phiwa <phiwen2@gmail.com>
2021-11-23 23:32:21 +0200

    redirect with query vars

--- src/templates/project-details.jsx
+++ src/templates/project-details.jsx
@@ -2,20 +2,45 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Img from 'gatsby-image'
 import * as styles from '../styles/project-details.module.css'
+import { graphql } from 'gatsby';
 
-const ProjectDetails = () => {
+const ProjectDetails = ({ data }) => {
+    const { html } = data.markdownRemark;
+    const { title, stack, featured } = data.markdownRemark.frontmatter;
+
     return (
         <Layout>
             <div className={styles.details}>
-                <h2>title</h2>
-                <h3>stack</h3>
+                <h2>{title}</h2>
+                <h3>{stack}</h3>
                 <div className={styles.featured}>
-                    {/* <Img fluid={}/> */}
+                    <Img fluid={featured.childImageSharp.fluid}/>
                 </div>
-                {/* <div className={styles.html} dangerouslySetInnerHTML={}/> */}
+                <div className={styles.html} dangerouslySetInnerHTML={{__html: html}}/>
             </div>
         </Layout>
     );
 }
 
 export default ProjectDetails;
+
+
+export const query = graphql`
+query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        stack
+        title
+        featured {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+  
+`
