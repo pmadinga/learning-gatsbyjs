dc46a92f436e6940c1421406bc2f1402092a8a0c
Phiwa <phiwen2@gmail.com>
2021-11-23 21:40:58 +0200

    Added thumbnails on ui

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
 import * as styles from '../../styles/projects.module.css'
+import Img  from 'gatsby-image';
 
 const Projects = ({data}) => {
     console.log(data);
@@ -17,6 +18,7 @@ const Projects = ({data}) => {
                     {projects.map(project => (
                         <Link to={"/projects" + project.frontmatter.slug} key={project.id}>
                             <div>
+                                <Img fluid={project.frontmatter.thumb.childImageSharp.fluid}/>
                                 <h3>{project.frontmatter.title}</h3>
                                 <p>{project.frontmatter.stack}</p>
                             </div>
@@ -40,14 +42,21 @@ query ProjectsPage {
           title
           stack
           slug
+          thumb {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
         }
         id
       }
     }
     contact: site {
-        siteMetadata{
-            contact
-        }
+      siteMetadata {
+        contact
+      }
     }
   }
   
